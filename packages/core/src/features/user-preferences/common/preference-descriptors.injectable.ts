ac917f817b1bbc6ed2c355d3b2cb0848954a67c8
radoslav s <radoslav993@gmail.com>
2025-04-20 15:40:09 +0200

    Implement hotbar cells configuration and preferences management
    - Added functionality to set and manage default hotbar cells.
    - Introduced new components for user preferences related to hotbar cells.
    - Updated hotbar rendering logic to respect the configured number of cells.

--- packages/core/src/features/user-preferences/common/preference-descriptors.injectable.ts
+++ packages/core/src/features/user-preferences/common/preference-descriptors.injectable.ts
@@ -12,6 +12,7 @@ import { defaultThemeId } from "../../../common/vars";
 import currentTimezoneInjectable from "../../../common/vars/current-timezone.injectable";
 import type { EditorConfiguration, ExtensionRegistry, KubeconfigSyncEntry, KubeconfigSyncValue, TerminalConfig } from "./preferences-helpers";
 import { defaultExtensionRegistryUrlLocation, defaultEditorConfig, defaultTerminalConfig, defaultPackageMirror, getPreferenceDescriptor, packageMirrors } from "./preferences-helpers";
+import { defaultHotbarCells, minHotbarCells } from "../../hotbar/storage/common/types";
 
 export type PreferenceDescriptors = ReturnType<typeof userPreferenceDescriptorsInjectable["instantiate"]>;
 
@@ -25,6 +26,12 @@ const userPreferenceDescriptorsInjectable = getInjectable({
     const mainKubeFolderPath = joinPaths(homeDirectoryPath, ".kube");
 
     return ({
+      hotbarCells: getPreferenceDescriptor<number>({
+        fromStore: (val) => val ?? defaultHotbarCells,
+        toStore: (val) => val === defaultHotbarCells || val < minHotbarCells
+          ? undefined
+          : val,
+      }),
       httpsProxy: getPreferenceDescriptor<string | undefined>({
         fromStore: val => val,
         toStore: val => val || undefined,
