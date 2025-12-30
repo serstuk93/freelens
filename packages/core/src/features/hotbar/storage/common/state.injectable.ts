2c1883708113ff791656e99b8fe1d73054c613ed
serstuk93 <radoslav993@gmail.com>
2025-12-30 20:13:06 +0100

    autosync of changed amount of cells

--- packages/core/src/features/hotbar/storage/common/state.injectable.ts
+++ packages/core/src/features/hotbar/storage/common/state.injectable.ts
@@ -7,11 +7,46 @@
 import { getInjectable } from "@ogre-tools/injectable";
 import { observable } from "mobx";
 
+import { reaction } from "mobx";
+import userPreferencesStateInjectable from "../../../user-preferences/common/state.injectable";
+import { defaultHotbarCells } from "./types";
+
 import type { Hotbar } from "./hotbar";
 
 const hotbarsStateInjectable = getInjectable({
   id: "hotbars-state",
-  instantiate: () => observable.map<string, Hotbar>(),
+  instantiate: (di) => {
+    const state = observable.map<string, Hotbar>();
+    const userPreferencesState = di.inject(userPreferencesStateInjectable);
+
+    // Ensure hotbar items length matches hotbarCells preference
+    reaction(
+      () => userPreferencesState.hotbarCells,
+      (hotbarCells) => {
+        for (const hotbar of state.values()) {
+          ensureExactHotbarItemLength(hotbar, hotbarCells ?? defaultHotbarCells);
+        }
+      },
+      { fireImmediately: true }
+    );
+
+    return state;
+  },
 });
 
 export default hotbarsStateInjectable;
+
+
+function ensureExactHotbarItemLength(hotbar: Hotbar, hotbarCells: number) {
+  while (hotbar.items.length < hotbarCells) {
+    hotbar.items.push(null);
+  }
+  while (hotbar.items.length > hotbarCells) {
+    const lastNull = hotbar.items.lastIndexOf(null);
+    if (lastNull >= 0) {
+      hotbar.items.splice(lastNull, 1);
+    } else {
+      hotbar.items.length = hotbarCells;
+    }
+  }
+}
